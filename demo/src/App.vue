dbcf84c3b522389e1d0df24b3dfc5445471c130c
gliburch <studio.gliburch@gmail.com>
2022-07-21 09:18:06 +0900

    Layout preview area

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="@/assets/logo.png">
-    <hello-world msg="Welcome to Your Vue.js App"></hello-world>
+    <div class="about">
+      <img alt="Vue logo" src="@/assets/logo.png">
+      <hello-world msg="Welcome to Your Vue.js App"></hello-world>
+    </div>
+    <div class="preview">
+      <br><br><br>
+      (채팅 컴포넌트 배치)
+    </div>
   </div>
 </template>
 
@@ -16,13 +22,26 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+.about {
+  width: 50%;
+  padding: 100px 0;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.preview {
+  position: fixed;
+  top: 50%;
+  left: 75%;
+  width: 375px;
+  height: 667px;
+  background-color: #f5f5f5;
+  text-align: center;
+  transform: translate(-50%, -50%);
 }
 </style>
