1612ced530e07a2a2fb98625d3d3c9564ad00cd6
Bo-Mi Kim <bomi@Bo-Mis-MacBook-Pro.local>
2022-07-28 20:14:45 +0900

    filePond  component  이동

--- demo/src/components/MessageInput.vue
+++ demo/src/components/MessageInput.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
     <!-- File Upload -->
-       <file-pond
-      name="test"
-      ref="pond"
-      label-idle="Drop files here..."
-      v-bind:allow-multiple="true"
-      accepted-file-types="image/jpeg, image/png"
-      v-bind:files="file"
-      v-on:init="handleFilePondInit"
-    />
-
     <!-- Photo -->
     <!-- Emoji -->
     <textarea
@@ -27,25 +17,7 @@
 
 <script>
 import { SendbirdAction } from '@/sendbird/SendbirdAction'
- // Import Vue FilePond
-import vueFilePond from "vue-filepond";
 
-// Import FilePond styles
-import "filepond/dist/filepond.min.css";
-
-// Import image preview plugin styles
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
-
-// Import image preview and file type validation plugins
-import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import FilePondPluginImageEdit from "filepond-plugin-image-edit";
-// Create component
-const FilePond = vueFilePond(
-  FilePondPluginFileValidateType,
-  FilePondPluginImagePreview,
-  FilePondPluginImageEdit
-);
 
 export default {
   name: 'MessageInput',
@@ -80,13 +52,9 @@ export default {
         })
     },
 */
-     handleFilePondInit: function () {
-      console.log("FilePond has initialized");
-    },
+
 },
-components: {
-  FilePond,
-},
+
 };
 
 </script>
