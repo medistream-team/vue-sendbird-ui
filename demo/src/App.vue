abbe5b9fce5e2862208d653c428cfa295211cf5c
Bomi Kim <77920302+rlaqhalx@users.noreply.github.com>
2022-08-01 12:59:59 +0900

    Revert "Feature/file upload"

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,65 +1,28 @@
 <template>
   <div id="app">
-    <div class="about">
-      
-      <button type="button" @click="toggleSortDirection()">
-        방향 토글하기 인 ({{ sortDirection }})
-      </button>
-
-      <img alt="Vue logo" src="@/assets/logo.png">
-      
-    </div>
-    <div class="preview">
-      <message-widget
-        :sort-direction="sortDirection"
-      ></message-widget>
-    </div>
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <hello-world msg="Welcome to Your Vue.js App"></hello-world>
   </div>
 </template>
 
 <script>
-
-import MessageWidget from './components/MessageWidget.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    MessageWidget
-  },
-   data() {
-    return {
-      sortDirection: "top",
-    };
-  },
-  methods: {
-    toggleSortDirection: function () {
-      this.sortDirection = this.sortDirection === "top" ? "bottom" : "top";
-    },
-  },
+    HelloWorld
+  }
 }
 </script>
 
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-.about {
-  width: 50%;
-  padding: 100px 0;
   text-align: center;
   color: #2c3e50;
-}
-.preview {
-  overflow: auto;
-  position: fixed;
-  top: 50%;
-  left: 75%;
-  width: 375px;
-  height: 667px;
-  background-color: #f5f5f5;
-  text-align: center;
-  transform: translate(-50%, -50%);
+  margin-top: 60px;
 }
 </style>
